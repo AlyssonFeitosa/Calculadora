f623f5413c5bc1b8af43bea87121b0f62fba1c5e
AlyssonFeitosa <alyssonfeitosa2016x@gmail.com>
2021-07-05 19:41:19 -0300

    test

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,14 +1,19 @@
+/* eslint-disable import/no-named-as-default */
 /**
  * @format
  */
 
 import 'react-native';
 import React from 'react';
+import renderer from 'react-test-renderer';
+// eslint-disable-next-line import/no-named-as-default-member
+// eslint-disable-next-line import/no-named-as-default
+// eslint-disable-next-line import/no-named-as-default-member
 import App from '../App';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
 
+// eslint-disable-next-line no-undef
 it('renders correctly', () => {
   renderer.create(<App />);
 });
